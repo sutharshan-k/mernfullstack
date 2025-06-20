5415c68db46781caeb6aff97c6f1f95b24c30306
Sutharshan <sutharshank107@gmail.com>
2025-06-20 16:23:01 +0530

    javascript backend

---
+++ Day4/day4.js
@@ -0,0 +1,55 @@
+console.log("Hello World");
+console.warn("warning");
+console.error("error");
+console.table([{name:"Alice",age:"20"},{name:"Bob",age:"21"}])
+
+eval("let a=5;console.log(\"Hello World\");console.log(a);");
+ 
+let x=3.14;
+console.log(x);
+
+let obj = {name:"John"};
+let str = JSON.stringify(obj);
+console.log(str);
+let back=JSON.parse(str);
+console.log(back);
+
+localStorage.setItem("username","Siva");
+console.log(localStorage.getItem("username"))
+
+sessionStorage.setItem("firstname","John");
+sessionStorage.setItem("age","20");
+console.log(sessionStorage.getItem("firstname"));
+
+let promise=new Promise((resolve)=>{
+    let isReady = true;
+    setTimeout(()=>{resolve("hello")},10000);
+});
+promise.then(res=>console.log(res)).catch(err=>console.log(err));
+
+function outer(){
+    let name="JS"
+    return function inner(){
+        console.log(name);
+    }
+}
+
+let dom=document.getElementById("DOM");
+let body = document.querySelector("div");
+
+dom.style.color="red";
+dom.innerText="Hello World";
+
+let newElement=document.createElement("h1");
+newElement.innerText="rithi";
+dom.appendChild(newElement);
+setTimeout(()=>{
+    dom.removeChild(newElement);},1000);
+
+
+let obj2=document.getElementById("DOM");
+obj2.addEventListener("click",()=>{
+    let obj1=document.getElementById("head");
+    console.log(obj1);
+    obj1.innerText="clicked";
+});
